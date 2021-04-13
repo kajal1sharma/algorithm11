abada1eb4762256ade48c8007aeb73a6d8d99f4b
kajal1sharma <kajal1231995@gmail.com>
2021-04-13 12:26:23 +0530

    13/04

---
+++ recursion/tailrec.cpp
@@ -0,0 +1,24 @@
+//tail recurison
+#include <iostream>
+
+using namespace std;
+
+void tailrec(int n){
+    if(n==0){
+        return;
+    }
+    else{
+    int a=90;
+    a=a+n;
+    cout<<a<<" "<<n<<endl;//task
+    //logic is before the recursive call
+    tailrec(n-1);//recurdsion//if this call is after the task =>tail recursion
+    }
+}
+
+int main()
+{
+    int n=3;
+    tailrec(n);
+    return 0;
+}
